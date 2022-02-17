268879ece1407774f8656fb1892764e50fd85cde
ratnaji <ratnajiguptha819@gmail.com>
2022-02-17 09:10:43 +0000

    command add

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
-const app = require("express")
-con
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const addDate = require("date-fns");
+
+const dbPath = path.join(__dirname, "todoApplication.db");
+const app = express();
+
+let db = null;
+
+const initializationDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error : ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializationDbAndServer();
+
+const convertTodoDbToResponseObject = (dbObject) => {
+  return {
+    id: dbObject.id,
+    todo: dbObject.todo,
+    category: dbObject.category,
+    priority: dbObject.priority,
+    status: dbObject.status,
+    dueDate: dbObject.due_date,
+  };
+};
+
+app.get("/todos/:todoId", async (request, response) => {
+  const { todoId } = request.params;
+  const getTodoQuery = `SELECT * FROM todo WHERE id = ${todoId}`;
+  const todoDetails = await db.get(getTodoQuery);
+  response.send(convertTodoDbToResponseObject(todoDetails));
+});
